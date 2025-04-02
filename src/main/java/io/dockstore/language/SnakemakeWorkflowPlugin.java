a3ebcff210a526399a60ac1cc21e52c8090a4177
Denis Yuen <denis.yuen@oicr.on.ca>
2025-04-02 10:33:41 -0400

    Snakemake prototype support (#2)
    * prototype with rule listin
    * add a few more directories, note where we need recursion
    * survive empty directory, add actual file content
    * [maven-release-plugin] prepare release snakemake-language-interface-0.0.1
    * [maven-release-plugin] prepare for next development iteration
    * [maven-release-plugin] prepare release snakemake-language-interface-0.0.2
    * [maven-release-plugin] prepare for next development iteration
    * make everything absolute
    * [maven-release-plugin] prepare release snakemake-language-interface-0.0.3
    * [maven-release-plugin] prepare for next development iteration

---
+++ src/main/java/io/dockstore/language/SnakemakeWorkflowPlugin.java
@@ -0,0 +1,181 @@
+/*
+ *    Copyright 2025 OICR and UCSC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package io.dockstore.language;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import io.dockstore.common.DescriptorLanguage;
+import io.dockstore.common.VersionTypeValidation;
+import org.pf4j.Extension;
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author dyuen
+ */
+public class SnakemakeWorkflowPlugin extends Plugin {
+
+    public static final Logger LOG = LoggerFactory.getLogger(SnakemakeWorkflowPlugin.class);
+    public static final String SNAKEMAKE_WORKFLOW_CATALOG_YML = "/.snakemake-workflow-catalog.yml";
+
+
+    /**
+     * Constructor to be used by plugin manager for plugin instantiation. Your plugins have to provide
+     * constructor with this exact signature to be successfully loaded by manager.
+     *
+     * @param wrapper
+     */
+    public SnakemakeWorkflowPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Extension
+    public static class SnakemakeWorkflowPluginImpl implements RecommendedLanguageInterface {
+
+        @Override
+        public String launchInstructions(String trsID) {
+            return null;
+        }
+
+
+        @Override
+        public VersionTypeValidation validateWorkflowSet(String initialPath, String contents, Map<String, FileMetadata> indexedFiles) {
+            VersionTypeValidation validation = new VersionTypeValidation(indexedFiles.containsKey(SNAKEMAKE_WORKFLOW_CATALOG_YML), new HashMap<>());
+            // TODO hook up some real validation
+            return validation;
+        }
+
+        @Override
+        public VersionTypeValidation validateTestParameterSet(Map<String, FileMetadata> indexedFiles) {
+            return new VersionTypeValidation(true, new HashMap<>());
+        }
+
+
+        @Override
+        public DescriptorLanguage getDescriptorLanguage() {
+            return DescriptorLanguage.SMK;
+        }
+
+        /**
+         * TODO: non-standardized Snakemake files can technically have their Snakefile in the root, supporting this probably
+         * affects some assumptions about folder structure below https://github.com/snakemake/snakemake-workflow-catalog?tab=readme-ov-file#standardized-usage-workflows
+         * @return
+         */
+        @Override
+        public Pattern initialPathPattern() {
+            return Pattern.compile("workflow/Snakefile");
+        }
+
+        @Override
+        public Map<String, FileMetadata> indexWorkflowFiles(String initialPath, String contents, FileReader reader) {
+            Map<String, FileMetadata> results = new HashMap<>();
+            // start with the workflow/Snakemake, some notes on workflow structure at https://snakemake.readthedocs.io/en/stable/snakefiles/deployment.html
+            results.put(
+                initialPath,
+                //TODO: get real snakemake version number, if they have one
+                new FileMetadata(contents, GenericFileType.IMPORTED_DESCRIPTOR, "1.0"));
+            // there can be  licenses, readme files and other useful stuff in the root
+            processFolder("/", null, reader, results);
+
+
+            // Snakefile might be in same directory as catalog file or up in a workflow directory (a workflow directory will have other folders), is not named in the catalog file
+            processFolder(initialPath, "/workflow", reader, results);
+            if (results.containsKey("/workflow/Snakefile")) {
+                processWorkflowFolders(reader, results);
+            }
+            // sometimes there is a config folder
+            processFolder(initialPath, "/config", reader, results);
+            // there are results and resources folders, developers are encouraged to keep the latter small
+            // processFolder(initialPath, "results", reader, results);
+            processFolder(initialPath, "/resources", reader, results);
+
+            /// TODO modules support
+
+            // workflow hub extensions
+            // TODO will need to be recursive
+            processFolder(initialPath, ".tests", reader, results);
+
+
+            return results;
+        }
+
+        private void processWorkflowFolders(FileReader reader, Map<String, FileMetadata> results) {
+            processFolder("/workflow", "/workflow/envs", reader, results);
+            processFolder("/workflow", "/workflow/report", reader, results);
+            processFolder("/workflow", "/workflow/rules", reader, results);
+            // TODO see comment in https://github.com/dockstore/snakemake-language-interface/pull/2#discussion_r2014758910
+            // the schemas directory may be optional and potentially added by a `validate` call, investigate
+            processFolder("/workflow", "/workflow/schemas", reader, results);
+            processFolder("/workflow", "/workflow/scripts", reader, results);
+            processFolder("/workflow", "/workflow/notebooks", reader, results);
+        }
+
+        // TODO determine how to process non-descriptor content https://ucsc-cgl.atlassian.net/browse/DOCK-262
+        private void processFolder(String initialPath, String folder, FileReader reader, Map<String, FileMetadata> results) {
+            List<String> files = findFiles(initialPath, folder, reader);
+            for (String file : files) {
+                results.put(file, new FileMetadata(reader.readFile(file), GenericFileType.IMPORTED_DESCRIPTOR, null));
+            }
+        }
+
+        // TODO: folderToCheck should probably be dealt with recursively
+        // TODO: use typed files instead of FileUtils from Apache (Galaxy plugin)
+        protected List<String> findFiles(final String initialPath, final String folderToCheck, final FileReader reader) {
+
+            final int extensionPos = initialPath.lastIndexOf("/");
+            final String base = initialPath.substring(0, extensionPos);
+
+            final Path pathsInDir = folderToCheck == null ? Paths.get(base) : Paths.get(base, folderToCheck);
+            // listing files is more rate limit friendly (e.g. GitHub counts each 404 "miss" as an API
+            // call,
+            // but listing a directory can be free if previously requested/cached)
+            List<String> filennamesInDir = reader.listFiles(pathsInDir.toString());
+            if (filennamesInDir == null) {
+                return Lists.newArrayList();
+            }
+            final Set<String> filenameSet = Sets.newHashSet(filennamesInDir);
+            return filenameSet.stream().map(s ->  (folderToCheck == null ? "/" : folderToCheck + "/") + s).toList();
+        }
+
+        @Override
+        public WorkflowMetadata parseWorkflowForMetadata(String initialPath, String contents, Map<String, FileMetadata> indexedFiles) {
+            WorkflowMetadata metadata = new WorkflowMetadata();
+            // TODO: grab real metadata, this is carried over from SWL and is non-functional
+            for (String line : contents.split("\\r?\\n")) {
+                if (line.startsWith("author")) {
+                    final String[] s = line.split(":");
+                    metadata.setAuthor(s[1].trim());
+                }
+                if (line.startsWith("description")) {
+                    final String[] s = line.split(":");
+                    metadata.setDescription(s[1].trim());
+                }
+            }
+            return metadata;
+        }
+    }
+}
+
---
+++ src/test/java/io/dockstore/language/SnakemakeWorkflowPluginTest.java
@@ -0,0 +1,130 @@
+package io.dockstore.language;
+
+import static io.dockstore.language.SnakemakeWorkflowPlugin.SNAKEMAKE_WORKFLOW_CATALOG_YML;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.io.Resources;
+import io.dockstore.common.VersionTypeValidation;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+public class SnakemakeWorkflowPluginTest {
+
+    @Test
+    public void testWorkflowParsingHelloWorldWithoutCatalog() {
+        VersionTypeValidation versionTypeValidation = attemptValidation("nathanhaigh/snakemake-hello-world", false);
+        assertFalse(versionTypeValidation.isValid());
+    }
+
+    private static VersionTypeValidation attemptValidation(String repoName, boolean isCatalogPresent) {
+        final SnakemakeWorkflowPlugin.SnakemakeWorkflowPluginImpl plugin =
+            new SnakemakeWorkflowPlugin.SnakemakeWorkflowPluginImpl();
+        final ResourceFileReader reader = new ResourceFileReader(repoName);
+        final String initialPath = "/workflow/Snakefile";
+        final String contents = reader.readFile(initialPath);
+        final Map<String, MinimalLanguageInterface.FileMetadata> fileMap =
+            plugin.indexWorkflowFiles(initialPath, contents, reader);
+
+        // TODO parse some actual information
+        plugin.parseWorkflowForMetadata(initialPath, contents, fileMap);
+
+        assertFalse(fileMap.isEmpty());
+        assertEquals(fileMap.containsKey(SNAKEMAKE_WORKFLOW_CATALOG_YML), isCatalogPresent);
+        assertTrue(fileMap.containsKey("/README.md"));
+        return plugin.validateWorkflowSet(initialPath, contents, fileMap);
+    }
+
+    @Test
+    public void testWorkflowParsingHelloWorld() {
+        VersionTypeValidation versionTypeValidation = attemptValidation("nathanhaigh/snakemake-hello-world-with-catalog", true);
+        assertTrue(versionTypeValidation.isValid());
+    }
+
+    @Test
+    public void testWorkflowParsingComplex() {
+        final SnakemakeWorkflowPlugin.SnakemakeWorkflowPluginImpl plugin =
+            new SnakemakeWorkflowPlugin.SnakemakeWorkflowPluginImpl();
+        final ResourceFileReader reader = new ResourceFileReader("snakemake-workflows/rna-seq-star-deseq2");
+        final String initialPath = "/workflow/Snakefile";
+        final String contents = reader.readFile(initialPath);
+        final Map<String, MinimalLanguageInterface.FileMetadata> fileMap =
+            plugin.indexWorkflowFiles(initialPath, contents, reader);
+
+        assertFalse(fileMap.isEmpty());
+        assertTrue(fileMap.containsKey("/workflow/rules/align.smk"));
+        // TODO recursion, may need to update the plugin handler in dockstore itself
+        // assertTrue(fileMap.containsKey(".test/config_basic/config.yaml"));
+        assertTrue(fileMap.containsKey("/LICENSE"));
+        assertTrue(fileMap.get("/LICENSE").content().contains("The above copyright notice and this permission notice shall be included in all"));
+        assertTrue(fileMap.get("/workflow/rules/align.smk").content().contains("3.5.3/bio/star/align"));
+        assertTrue(plugin.validateWorkflowSet(initialPath, contents,fileMap).isValid());
+    }
+
+    abstract static class URLFileReader implements MinimalLanguageInterface.FileReader {
+        // URL to repo
+        protected final String repo;
+        // extracted ID
+        protected final Optional<String> id;
+
+        URLFileReader(final String repo) {
+            this.repo = repo;
+            final String[] split = repo.split("/");
+            if (split.length >= 2) {
+                id = Optional.of(split[split.length - 2] + "/" + split[split.length - 1]);
+            } else {
+                id = Optional.empty();
+            }
+        }
+
+        protected abstract URL getUrl(final String path) throws IOException;
+
+        @Override
+        public String readFile(String path) {
+            try {
+                if (path.startsWith("/")) {
+                    path = path.substring(1);
+                }
+                URL url = this.getUrl(path);
+                return Resources.toString(url, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public List<String> listFiles(String pathToDirectory) {
+            if (id.isEmpty()) {
+                return new ArrayList<>();
+            }
+            String fileDirectory = "src/test/resources/io/dockstore/language/" + this.id.get() + "/" + pathToDirectory;
+            return FileUtils.listFiles(new File(fileDirectory), null, false).stream().map(f -> f.getName()).toList();
+        }
+    }
+
+    static class ResourceFileReader extends URLFileReader {
+
+        ResourceFileReader(final String repo) {
+            super(repo);
+        }
+
+        @Override
+        protected URL getUrl(String path) throws IOException {
+            final String classPath = this.repo + "/" + path;
+            final URL url = SnakemakeWorkflowPluginTest.class.getResource(classPath);
+            if (url == null) {
+                throw new IOException("No such file " + classPath);
+            }
+            return url;
+        }
+    }
+}
